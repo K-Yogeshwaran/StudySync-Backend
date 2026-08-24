d2d999081d8386e5302673aecd718f73ca7a81a2
Yogeshwaran K <yogeshkuberan111@gmail.com>
2026-08-24 13:11:06 +0530

    Initial project setup

---
+++ src/main/java/space/yogeshwarank/studysync/StudysyncBackendApplication.java
@@ -0,0 +1,13 @@
+package space.yogeshwarank.studysync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudysyncBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudysyncBackendApplication.class, args);
+    }
+
+}
