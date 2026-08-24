da02e2b815c6b63c38984c3612f719a9b6b30c7b
Yogeshwaran K <yogeshkuberan111@gmail.com>
2026-08-24 15:00:28 +0530

    ci : add backend build and test pipeline

--- src/test/java/space/yogeshwarank/studysync/controller/HealthControllerTest.java
+++ src/test/java/space/yogeshwarank/studysync/controller/HealthControllerTest.java
@@ -1,4 +1,19 @@
 package space.yogeshwarank.studysync.controller;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HealthControllerTest {
+
+    @Test
+    public void healthShouldReturnUp(){
+        HealthController healthController = new HealthController();
+
+        var response = healthController.getHealthStatus();
+
+        assertEquals("UP", response.get("status"));
+        assertEquals("studysync-backend", response.get("service"));
+    }
 }
